2edd3035b7c4fb1d09230272e2b155c33b5bcb92
cxreiff <cooper@cxreiff.com>
2023-06-12 14:40:31 -0700

    cargo fmt, added manual trigger to deploy action

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use bevy::prelude::*;
 
-mod config_plugin;
 mod camera_plugin;
-mod loading_plugin;
+mod config_plugin;
 mod kitty_plugin;
+mod loading_plugin;
 
-use config_plugin::ConfigPlugin;
-use loading_plugin::LoadingPlugin;
 use camera_plugin::CameraPlugin;
+use config_plugin::ConfigPlugin;
 use kitty_plugin::KittyPlugin;
+use loading_plugin::LoadingPlugin;
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash, Default, States)]
 enum GameState {
